2365ebb97ab64b5732876748e13fcf48f4cef9cc
Dhairya Jadav <jadavdhairya@gmail.com>
2024-02-04 19:55:21 -0700

    Added code for the basic structure of the game

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # odin-etch-a-sketch
+
+### This is a basic representation of an etch-a-sketch.
+### Currently the game displays a color when the mouse hovers over a grid square.
+### A button is added to change the size of the grid as per user's request.
+
+## ToDo:
+
+### Transform the behavior of a square when interacting with the mouse by introducing a series of modifications.
+
+### Rather than a color change from black to white, each interaction should randomize the square’s RGB value entirely.
+### Additionally, implement a progressive darkening effect where each interaction adds 10% more black or color to the square. The objective is to achieve a completely black square only after ten interactions.
---
+++ index.js
@@ -0,0 +1,99 @@
+const header = document.createElement("div");
+const title = document.createElement("h1");
+const gridBtn = document.createElement("button");
+
+let defaultSize = 16;
+
+title.textContent = "This is an Etch-A-Sketch game"
+gridBtn.textContent = "Change Grid Size";
+header.classList.add("header");
+gridBtn.classList.add("grid-button");
+
+header.appendChild(title);
+header.appendChild(gridBtn);
+
+gridBtn.addEventListener("click", () => {
+    defaultSize = parseInt(prompt("Enter the total squares in the grid between 1 and 64: "), 10);
+
+    if (!isNaN(defaultSize) && defaultSize > 0 && defaultSize <= 64) {
+        updateGrid(defaultSize);
+    } else {
+        alert("Please enter a valid positive number for the grid size.");
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const container = createGrid(defaultSize);
+    document.body.appendChild(header);
+    document.body.appendChild(container);
+});
+
+function createGrid(defaultSize) {
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    let containerSize = getStyleRuleValue('.container', 'width');
+    containerSize = containerSize.slice(0, -2);
+    containerSize = parseInt(containerSize);
+
+    let newBoxSize = (containerSize / defaultSize);
+    newBoxSize = newBoxSize.toString() + "px";
+
+    for (let i = 0; i < defaultSize * defaultSize; i++) {
+        const square = document.createElement("div");
+        square.classList.add("squares");
+        container.appendChild(square);
+    }
+
+    const squareCssRule = getStyleRule('.squares');
+
+    squareCssRule.style['width'] = newBoxSize;
+    squareCssRule.style['height'] = newBoxSize;
+
+    return container;
+}
+
+function updateGrid(newSize) {
+    const container = document.querySelector(".container");
+    document.body.removeChild(container);
+
+    const newContainer = createGrid(newSize);
+    document.body.appendChild(newContainer);
+}
+
+function getStyleRuleValue(selector, style) {
+    var selector_compare = selector.toLowerCase();
+    var selector_compare2 = selector_compare.substr(0, 1) === '.' ? selector_compare.substr(1) : '.' + selector_compare;
+
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        var mysheet = document.styleSheets[i];
+        var myrules = mysheet.cssRules ? mysheet.cssRules : mysheet.rules;
+
+        for (var j = 0; j < myrules.length; j++) {
+            if (myrules[j].selectorText) {
+                var check = myrules[j].selectorText.toLowerCase();
+                switch (check) {
+                    case selector_compare:
+                    case selector_compare2: return myrules[j].style[style];
+                }
+            }
+        }
+    }
+}
+
+function getStyleRule(selector) {
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        var mysheet = document.styleSheets[i];
+        var myrules = mysheet.cssRules ? mysheet.cssRules : mysheet.rules;
+
+        for (var j = 0; j < myrules.length; j++) {
+            if (myrules[j].selectorText) {
+                var check = myrules[j].selectorText.toLowerCase();
+                switch (check) {
+                    case selector: return myrules[j];
+                }
+            }
+        }
+    }
+}
